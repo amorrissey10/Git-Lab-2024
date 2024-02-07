0e643437141c8aaecb9797e04f1bf8d329abeb77
Aaron Morrissey <amorrissey10@huskers.unl.edu>
2024-02-07 12:00:49 -0600

    step 7

--- GitLab.java
+++ GitLab.java
@@ -1,8 +1,8 @@
 
     public class GitLab {
         public static void main(String[] args) {
-            GitLabHelper helper = new GitLabHelper(new String[] {"Ava H",
-"Aaron M"});
+            GitLabHelper helper = new GitLabHelper(new String[] {"Ava Hollingsworth",
+"Aaron Morrissey"});
 helper.PrintWelcomeMessage();
     }
 }
