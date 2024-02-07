ca764eb3eff88a3cde34b2d5a949dc4f53965bc7
ahollingsworth1 <112013499+ahollingsworth1@users.noreply.github.com>
2024-02-07 11:44:45 -0600

    step 6!

--- GitLab.java
+++ GitLab.java
@@ -1,1 +1,6 @@
-System.out.println("This is a lesson on git.")
+public class GitLab { 
+    public static void main(String[] args){
+        GitLabHelper helper = new GitLabHelper("Ava H", "Aaron M");
+        helper.PrintWelcomeMessage();
+}
+}
--- GitLabHelper.java
+++ GitLabHelper.java
@@ -1,5 +1,6 @@
-package edu.unl.raikes.git; public class GitLabHelper {
-private String person1; private String person2;
+public class GitLabHelper {
+private String person1; 
+private String person2;
 GitLabHelper(String person1, String person2) { this.person1 = person1;
 this.person2 = person2; }
      public void PrintWelcomeMessage() {
