0cf5be39a9510614fa9e3be2cecd65c2d76465bb
ahollingsworth1 <112013499+ahollingsworth1@users.noreply.github.com>
2024-02-07 11:55:43 -0600

    step 7!

--- GitLab.java
+++ GitLab.java
@@ -1,6 +1,8 @@
-public class GitLab { 
-    public static void main(String[] args){
-        GitLabHelper helper = new GitLabHelper("Ava H", "Aaron M");
-        helper.PrintWelcomeMessage();
-}
+
+    public class GitLab {
+        public static void main(String[] args) {
+            GitLabHelper helper = new GitLabHelper(new String[] {"Ava H",
+"Aaron M"});
+helper.PrintWelcomeMessage();
+    }
 }
